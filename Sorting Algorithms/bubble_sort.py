87dd2d18be295942828b50ca8c83793b058f022e
ZTBGaming <104875665+ZTBGaming@users.noreply.github.com>
2022-06-08 14:32:48 -0500

    Create bubble_sort.py

---
+++ Sorting Algorithms/bubble_sort.py
@@ -0,0 +1,20 @@
+"""
+Skillsoft/Percipio Python Bootcamp
+Course #15 Implementing Sorting Algorithms
+Bubble Sort
+COMPLETED: 2/25/2022
+
+This sort works for both num lists and string lists
+
+Time Complexity:   O(n^2)
+Space Complexity:  O(1)
+"""
+
+def bubble_sort(original_list):
+    length = len(original_list)
+    for i in range(length-1, 0, -1):
+        for index in range(i):
+            if original_list[index] > original_list[index+1]:
+                original_list[index+1], original_list[index] = original_list[index], original_list[index+1]
+        print('Unsorted until index: {}'.format(i-1))
+        print(original_list)
