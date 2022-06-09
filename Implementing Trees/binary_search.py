f0721560ce9160ac8bb9bad43413160c8ef5f071
Zach <104875665+ZachBlack@users.noreply.github.com>
2022-06-09 13:37:41 -0500

    Update binary_search.py

--- Implementing Trees/binary_search.py
+++ Implementing Trees/binary_search.py
@@ -6,3 +6,16 @@ Binary Search
 
 COMPLETED: 3/6/2022
 """
+
+def binary_search(sorted_list, key):
+    min_index = 0
+    max_index = len(sorted_list)-1
+    while min_index <= max_index:
+        mid = (min_index + max_index)//2
+        if sorted_list[mid] == key:
+            return mid
+        elif sorted_list[mid] < key:
+            min_index = mid+1
+        else:
+            max_index = mid-1
+    return -1    
